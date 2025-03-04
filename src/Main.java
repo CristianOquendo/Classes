a26c94b239c2120fb653ea9195a8b4276a9064b0
Sala315 <cristian2354g@gmail.com>
2025-03-03 19:21:55 -0500

    Student Creation

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,28 @@ import javax.lang.model.element.Name;
 public class Main {
 
     public static void main(String[] args) {
-        Student Cristian  = new Student("Cristian", "000562863", 17, 'm'  );
-        Student Diego = new Student();
-        System.out.println(Cristian);
-        System.out.println(Diego);
-        System.out.println(Cristian.studentId);
-        System.out.println(Diego.studentId);
+        Student cristian  = new Student("Cristian", " 000562863", 17, 'm'  );
+        Student diego = new Student();
+        System.out.println(cristian);
+        System.out.println(diego);
+        System.out.println(cristian.studentId);
+        System.out.println(diego.studentId);
+
+        System.out.println("Has cumplido años, si o no ");
+        String respuesta = cristian.studentId;
+
+        if (respuesta.equals("Cristian")) {
+            cristian.haveBirthday();
+            System.out.println(" Feliz cumpleaños " + cristian.studentId + "ahora tienes" + cristian.studentAge);
+        }
+
+        cristian.tellInfo();
+        cristian.repeatAfter("Haw are you");
+        cristian.haveBirthday();
+
+        String cristianEmail = cristian.getEmail();
+        System.out.println(cristianEmail);
+
+
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,20 +1,54 @@
 public class Student {
     String studentName;
-    String studentId = "0000000" ;
+    String studentId = "0000000";
     int studentAge;
     char studentGender;
+    String studentEmail;
 
     // Constructor
     public Student() {
         System.out.println("Estudiante creado sin datos ");
     }
-    public Student (String name , String id, int age, char gender) {
+
+    public Student(String name, String id, int age, char gender) {
         studentName = name;
         studentId = id;
         studentAge = age;
         studentGender = gender;
+
         System.out.println("Estudiante \"" + studentName + "\" creado");
 
+    }
+
+    public Student(String name, String id, int age, char gender, String email) {
+        studentName = name;
+        studentId = id;
+        studentAge = age;
+        studentGender = gender;
+        studentEmail = email;
+        System.out.println("Estudiante \"" + studentName + "\" creado");
 
     }
+
+    public void tellInfo() {
+        System.out.println("My name is  \"" + studentName + "\" and my ID is" + studentId);
+        System.out.println("I am  " + studentAge + " years old and my gender is" + studentGender);
+    }
+
+    public void repeatAfter(String message) {
+        System.out.println("The message is " + message);
+    }
+
+    public void haveBirthday() {
+        studentAge++;
+    }
+
+    public String getEmail() {
+        return studentEmail;
+    }
+
+    public String toString() {
+        return "Student with name :" + studentName ;
+    }
+
 }
